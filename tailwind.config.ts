2f478e7b2a0a73b8d86189d4a7e0ecf62226b388
Soham Ghadigaonkar <sohamghadigaonkar29@gmail.com>
2025-09-19 12:30:26 +0000

    can you make the ui to look it cyber theam

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Space Grotesk', 'sans-serif'],
+        body: ['Space Grotesk', 'sans-serif'],
+        headline: ['Orbitron', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
